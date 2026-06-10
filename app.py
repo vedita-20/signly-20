d49348fa01994aa55b80eb3e98bfc35e74d19509
vedita-20 <veditamaini10@gmail.com>
2026-06-11 02:23:10 +0530

    deploy

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
